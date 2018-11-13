de37747f826069fc3b39bb8ef4d807cb9f99e753
doniseferi <doni.seferi@gmail.com>
2018-11-13 22:27:34 +0000

    added yatzyCollection

--- src/ICollection.ts
+++ src/ICollection.ts
@@ -1,8 +1,22 @@
 import Die from '../src/Die';
+import { isNullOrUndefined } from 'util';
 
-interface ICollection<T> {
-    collection: ICollection<Die>
-}
+export default class Dice {
+    
+    private readonly numberOfDieInYatzyCollection = 5;
 
-export default class Dice implements ICollection<Die> {
+    constructor(public dice: Die[]) {
+        this.validate(dice);
+    }
+
+    validate(dice: Die[]): any {
+        if (isNullOrUndefined(dice))
+            throw new Error('The Yatzy collection is null or undefined.');
+
+        if (dice.length !== this.numberOfDieInYatzyCollection)
+            throw new Error('A Yatzy collection needs exactly 5 die.');
+
+        if (dice.some(die => isNullOrUndefined(die)))
+            throw new Error('The Yatzy collection has a null or undefined value');
+    }
 }
