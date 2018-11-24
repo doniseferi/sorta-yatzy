5455afe4e889993939a0eaeec075632a819a998e
doniseferi <doni.seferi@gmail.com>
2018-11-24 22:21:31 +0000

    refactored straight.

--- src/Score/Straights/Straight.ts
+++ src/Score/Straights/Straight.ts
@@ -3,30 +3,53 @@ import { Dice } from "../../Dice";
 
 export abstract class Straight implements IScore {
 
-    private readonly shouldInclude: number;
-    private readonly shouldntInclude: number;
-    private readonly points: number;
+    private readonly requiredUniqueVariablesLength: number = 5;
+    private requiredValueInVariables: number = 0;
+    private requiredAbsenceFromVariables: number = 0;
+    private points: number = 0;
 
     constructor(protected readonly smallStraight: boolean) {
-        if (smallStraight) {
-            this.shouldInclude = 1;
-            this.shouldntInclude = 6;
-            this.points = 15;
-        } else {
-            this.shouldInclude = 6;
-            this.shouldntInclude = 1;
-            this.points = 20;
-        }
+        this.Initalise(smallStraight);
     }
 
     Score(collection: Dice): number {
         let die = collection.dice.map(x => x.value);
         let uniqueValues = [...new Set(die)];
-        return (
-            uniqueValues.length >= 5 &&
-            uniqueValues.includes(this.shouldInclude) &&
-            uniqueValues.includes(this.shouldntInclude) === false)
+        return (this.isStraight(uniqueValues))
             ? this.points
             : 0;
+    }
+
+    private isStraight(collection: number[]): boolean {
+        return (
+            this.isTheRightLength(collection) &&
+            this.hasTheRequiredValue(collection) &&
+            this.hasTheRequiredAbsence(collection)
+        );
+    }
+
+    private hasTheRequiredAbsence(collection: number[]): boolean {
+        return collection.includes(this.requiredAbsenceFromVariables) === false;
+    }
+
+    private hasTheRequiredValue(collection: number[]): boolean {
+        return collection.includes(this.requiredValueInVariables);
+    }
+
+    private isTheRightLength(collection: number[]): boolean {
+        return collection.length == this.requiredUniqueVariablesLength;
+    }
+
+    private Initalise(smallStraight: boolean) {
+        if (smallStraight) {
+            this.requiredValueInVariables = 1;
+            this.requiredAbsenceFromVariables = 6;
+            this.points = 15;
+        }
+        else {
+            this.requiredValueInVariables = 6;
+            this.requiredAbsenceFromVariables = 1;
+            this.points = 20;
+        }
     }
 }
