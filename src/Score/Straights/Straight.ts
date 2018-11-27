367ebbfd756788282135df336816535f44849175
Edonis Seferi <doni.seferi@gmail.com>
2018-11-27 13:40:40 +0000

    moded init to ctor

--- src/Score/Straights/Straight.ts
+++ src/Score/Straights/Straight.ts
@@ -4,12 +4,21 @@ import { Dice } from "../../Dice";
 export abstract class Straight implements IScore {
 
     private readonly requiredUniqueVariablesLength: number = 5;
-    private requiredValueInVariables: number = 0;
-    private requiredAbsenceFromVariables: number = 0;
-    private points: number = 0;
+    private readonly requiredValueInVariables: number = 0;
+    private readonly requiredAbsenceFromVariables: number = 0;
+    private readonly points: number = 0;
 
     constructor(protected readonly smallStraight: boolean) {
-        this.Initalise(smallStraight);
+        if (smallStraight) {
+            this.requiredValueInVariables = 1;
+            this.requiredAbsenceFromVariables = 6;
+            this.points = 15;
+        }
+        else {
+            this.requiredValueInVariables = 6;
+            this.requiredAbsenceFromVariables = 1;
+            this.points = 20;
+        }
     }
 
     Score(collection: Dice): number {
@@ -28,28 +37,15 @@ export abstract class Straight implements IScore {
         );
     }
 
-    private hasTheRequiredAbsence(collection: number[]): boolean {
-        return collection.includes(this.requiredAbsenceFromVariables) === false;
+    private hasTheRequiredAbsence(uniqueDieFaces: number[]): boolean {
+        return uniqueDieFaces.includes(this.requiredAbsenceFromVariables) === false;
     }
 
-    private hasTheRequiredValue(collection: number[]): boolean {
-        return collection.includes(this.requiredValueInVariables);
+    private hasTheRequiredValue(uniqueDieFaces: number[]): boolean {
+        return uniqueDieFaces.includes(this.requiredValueInVariables);
     }
 
-    private isTheRightLength(collection: number[]): boolean {
-        return collection.length == this.requiredUniqueVariablesLength;
-    }
-
-    private Initalise(smallStraight: boolean) {
-        if (smallStraight) {
-            this.requiredValueInVariables = 1;
-            this.requiredAbsenceFromVariables = 6;
-            this.points = 15;
-        }
-        else {
-            this.requiredValueInVariables = 6;
-            this.requiredAbsenceFromVariables = 1;
-            this.points = 20;
-        }
+    private isTheRightLength(unqiueDieFaces: number[]): boolean {
+        return unqiueDieFaces.length == this.requiredUniqueVariablesLength;
     }
 }
