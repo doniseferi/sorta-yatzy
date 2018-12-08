bc03f25dd12c68587cc6ad316ae2374763c7bab0
doniseferi <doni.seferi@gmail.com>
2018-12-08 23:00:03 +0000

    removed example ioc ninja example and test

--- src/ioc-example/types.ts
+++
@@ -1,8 +0,0 @@
-const TYPES = {
-    IWarrior: Symbol.for("Warror"),
-    IWeapon: Symbol.for("Weapon"),
-    IThrowableWeapon: Symbol.for("ThrowableWeapon"),
-    IYatzyFactory: Symbol.for("IYatzyFactory")
-};
-
-export { TYPES };
